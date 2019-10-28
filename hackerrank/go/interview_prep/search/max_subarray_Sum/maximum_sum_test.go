74e2640c36298c86a1e3ad488f73578a9c1f264b
Ties de Kock <ties@tiesdekock.nl>
2019-10-28 21:58:49 +0100

    Add another testcase

--- hackerrank/go/interview_prep/search/max_subarray_Sum/maximum_sum_test.go
+++ hackerrank/go/interview_prep/search/max_subarray_Sum/maximum_sum_test.go
@@ -11,4 +11,11 @@ func TestMaximumSum(t *testing.T) {
     if res != 6 {
         t.Error(fmt.Sprintf("sample input 0 result (%v) != 6", res))
     }
+
+    res = maximumSum([]int64{1,5,9}, 5)
+
+    if res != 4 {
+        t.Error(fmt.Sprintf("test case 18 result (%v) != 4", res))
+    }
+    res = maximumSum([]int64{3,2,7,4}, 7)
 }
